c715242750b4257cb7ab22165fcaeb960725c4e9
sothea-st <sotheakam210@gmail.com>
2025-05-24 15:10:08 +0700

    update application.yml of api-gateway

--- config-server/src/main/java/com/example/demo/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class UserServiceApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
